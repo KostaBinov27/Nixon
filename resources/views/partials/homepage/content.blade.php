0d4eb7078b8fc88e3107c985320950c4456b3b3a
kostabinov <kostabinovps@gmail.com>
2020-01-02 13:50:31 +0100

    Small changes on forms

--- resources/views/partials/homepage/content.blade.php
+++ resources/views/partials/homepage/content.blade.php
@@ -5,7 +5,7 @@
                 <div class="col-lg-7 mx-auto text-center">
                     <h1>Shopping online feels easeier and more fun</h1>
                     <p>Nixon is an online buying and selling site that has millions of online stores and sells various kinds of products to meet your needs.</p>
-                    <button type="button" class="btn btn-primary btnHero">Shop Now</button>
+                    <button type="button" class="btn btn-primary btnHero">Search</button>
                 </div>
                 <!-- <div class="col-lg-8"> -->
                     <!-- <img src="<?php// echo get_site_url(); ?>/wp-content/uploads/2019/12/headerImage.png" class="img-fluid"> -->
