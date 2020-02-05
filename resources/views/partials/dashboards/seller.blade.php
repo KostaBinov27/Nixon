c7c194435700002614d73ab4b494efe80b7b1c19
kostabinov <kostabinovps@gmail.com>
2020-02-05 14:02:41 +0100

    fixing Vendor

--- resources/views/partials/dashboards/seller.blade.php
+++ resources/views/partials/dashboards/seller.blade.php
@@ -11,6 +11,8 @@ $userInfo = $result_user -> fetch_assoc();
 $userInfo_meta = get_user_meta( $_SESSION['userID']);
 $vendor = get_wcmp_vendor($_SESSION['userID']);
 
+// print_r($vendor);
+
 if (isset($_POST['addNewProduct'])) {
 
         $current_date = date("Y-m-d");
@@ -41,7 +43,8 @@ if (isset($_POST['addNewProduct'])) {
         update_post_meta( $latestID, '_commission_per_product', '0' );
         update_post_meta( $latestID, '_download_limit', '-1' );
         update_post_meta( $latestID, '_download_expiry', '-1' );  
-        
+
+
         if ( !empty($_FILES['image']['name']) && empty($_FILES['image_hiden'])) {
             $dirPath = getcwd();
             require_once( $dirPath . '/wp-admin/includes/image.php' );
@@ -127,6 +130,17 @@ if (isset($_POST['addNewProduct'])) {
             $product->set_downloads($downloads);
             $product->save(); // Save the data in database
         }
+
+        wp_set_object_terms($latestID, absint($vendor->term_id), $WCMp->taxonomy->taxonomy_name);
+    }
+
+    $sql = "SELECT * FROM wp_usermeta WHERE user_id = '".$_SESSION['userID']."' AND meta_key = 'wp_capabilities'";
+    $result = mysqli_query($connWP, $sql);
+    $row = $result -> fetch_assoc();
+    
+    $sellerPendingActivation = 0;
+    if ($row['meta_value'] == 'a:1:{s:17:"dc_pending_vendor";b:1;}'){
+        $sellerPendingActivation = 1;
     }
 ?>
  
@@ -208,60 +222,72 @@ if (isset($_POST['addNewProduct'])) {
                     </form>
                 </div>
                 <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-                    <form method="POST" enctype="multipart/form-data">
-                        <h2>Add New Product</h2>
-                        <hr>
-                        <div class="form-row">
-                            <div class="form-group col-md-12">
-                                <label for="inputEmail4">Product Name</label>
-                                <input type="text" class="form-control" name="productName" id="inputEmail4" placeholder="" required>
-                            </div>
+                    <?php if ($sellerPendingActivation == 1) { ?>
+                        <div class="alert alert-warning" role="alert">
+                            Your account is still in review. Once your account is activated you will be able to post your products.
                         </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-12">
-                                <label for="inputEmail4">Product Description</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" name="product_description" rows="3"></textarea>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputAddress2">Regular price (€)</label>
-                                <input type="text" class="form-control" id="inputPrice" name="regularPrice" placeholder="e.g. 5.19" required>
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="inputAddress2">Sale price (€)</label>
-                                <input type="text" class="form-control" id="inputAddress2" placeholder="e.g. 2.10 or leave blank">
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputAddress2">Upload Product Image</label>
-                                
-                                <br>
-                                <div class="file-upload">
-                                    <label for="upload" class="file-upload__label">Upload Product Picture</label>
-                                    <input id="upload" name="image" class="file-upload__input" type="file">
-                                    <input type="hidden" name="image_hiden" multiple="false" />
+                    <?php } else { ?>
+                        <form method="POST" enctype="multipart/form-data">
+                            <h2>Add New Product</h2>
+                            <hr>
+                            <div class="form-row">
+                                <div class="form-group col-md-12">
+                                    <label for="inputEmail4">Product Name</label>
+                                    <input type="text" class="form-control" name="productName" id="inputEmail4" placeholder="" required>
                                 </div>
                             </div>
-                            <div class="form-group col-md-6">
-                                <label for="inputAddress2">Upload Product Image</label>
-                                
-                                <br>
-                                <div class="file-upload">
-                                    <label for="uploadFile" class="file-upload__label">Upload Product Picture</label>
-                                    <input id="uploadFile" name="product_file_custom" class="file-upload__input" type="file">
-                                    <input type="hidden" name="product_file_hiden" multiple="false" />
+                            <div class="form-row">
+                                <div class="form-group col-md-12">
+                                    <label for="inputEmail4">Product Description</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" name="product_description" rows="3"></textarea>
                                 </div>
                             </div>
-                        </div>
-                        <div class="form-row mt-5">
-                            <button type="submit" name="addNewProduct" class="btn btn-primary loginBTN mx-auto">Submit for Review</button>
-                        </div>
-                    </form>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress2">Regular price (€)</label>
+                                    <input type="text" class="form-control" id="inputPrice" name="regularPrice" placeholder="e.g. 5.19" required>
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress2">Sale price (€)</label>
+                                    <input type="text" class="form-control" id="inputAddress2" placeholder="e.g. 2.10 or leave blank">
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress2">Upload Product Image</label>
+                                    
+                                    <br>
+                                    <div class="file-upload">
+                                        <label for="upload" class="file-upload__label">Upload Product Picture</label>
+                                        <input id="upload" name="image" class="file-upload__input" type="file">
+                                        <input type="hidden" name="image_hiden" multiple="false" />
+                                    </div>
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="inputAddress2">Upload Product Image</label>
+                                    
+                                    <br>
+                                    <div class="file-upload">
+                                        <label for="uploadFile" class="file-upload__label">Upload Product Picture</label>
+                                        <input id="uploadFile" name="product_file_custom" class="file-upload__input" type="file">
+                                        <input type="hidden" name="product_file_hiden" multiple="false" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-row mt-5">
+                                <button type="submit" name="addNewProduct" class="btn btn-primary loginBTN mx-auto">Submit for Review</button>
+                            </div>
+                        </form>
+                    <?php } ?>
                 </div>
                 <div class="tab-pane fade" id="v-pills-new-product" role="tabpanel" aria-labelledby="v-pills-settings-tab">
-                    <table class="table table-striped" id="example"></table>
+                    <?php if ($sellerPendingActivation == 1) { ?>
+                        <div class="alert alert-warning" role="alert">
+                            Your account is still in review. Once your account is activated you will be able to post your products.
+                        </div>
+                    <?php } else { ?>
+                        <table class="table table-striped" id="example"></table>
+                    <?php } ?>    
                 </div>
                 <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">3</div>
                 <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">4</div>
